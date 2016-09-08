b4ba103edc0743ec41c5b30b7745ff749ab1300c
Justin Tran <justintran@wireless-10-147-119-173.public.utexas.edu>
2016-09-08 13:04:25 -0500

    Adding my tests

--- jdt2725-TestCollatz.py
+++ jdt2725-TestCollatz.py
@@ -22,118 +22,104 @@ from Collatz import collatz_read, collatz_eval, collatz_print, collatz_solve
 # -----------
 
 class TestCollatz(TestCase):
+    """Testing Collatz"""
     # ----
     # read
     # ----
 
-    def test_read(self):
+    def test_read_1(self):
+        """Testing read"""
         strg = "1 10\n"
         start, end = collatz_read(strg)
         self.assertEqual(start, 1)
         self.assertEqual(end, 10)
+
+    def test_read_2(self):
+        """Testing read"""
+        strg = "3 4\n"
+        start, end = collatz_read(strg)
+        self.assertEqual(start, 3)
+        self.assertEqual(end, 4)
+
+    def test_read_3(self):
+        """Testing read"""
+        strg = "77 80\n"
+        start, end = collatz_read(strg)
+        self.assertEqual(start, 77)
+        self.assertEqual(end, 80)
 
     # ----
     # eval
     # ----
 
     def test_eval_1(self):
+        """Testing eval"""
         val = collatz_eval(1, 10)
         self.assertEqual(val, 20)
 
     def test_eval_2(self):
+        """Testing eval"""
         val = collatz_eval(100, 200)
         self.assertEqual(val, 125)
 
     def test_eval_3(self):
+        """Testing eval"""
         val = collatz_eval(201, 210)
         self.assertEqual(val, 89)
 
     def test_eval_4(self):
+        """Testing eval"""
         val = collatz_eval(900, 1000)
         self.assertEqual(val, 174)
 
-    # ---------------
-    # more unit tests
-    # ---------------
-
     def test_eval_5(self):
-        val = collatz_eval(11, 1)
-        self.assertEqual(val, 20)
+        """Testing max cycle"""
+        val = collatz_eval(418515, 603657)
+        self.assertEqual(val, 470)
 
     def test_eval_6(self):
-        val = collatz_eval(350, 450)
-        self.assertEqual(val, 134)
+        """Testing max cycle"""
+        val = collatz_eval(132501, 322152)
+        self.assertEqual(val, 443)
 
     def test_eval_7(self):
-        val = collatz_eval(1, 3)
-        self.assertEqual(val, 8)
+        """Testing max cycle"""
+        val = collatz_eval(241267, 283022)
+        self.assertEqual(val, 407)
 
     def test_eval_8(self):
-        val = collatz_eval(200, 2300)
-        self.assertEqual(val, 183)
-
-    def test_eval_9(self):
-        val = collatz_eval(88, 89)
-        self.assertEqual(val, 31)
-
-    def test_eval_10(self):
-        val = collatz_eval(123, 456)
-        self.assertEqual(val, 144)
-
-    def test_eval_11(self):
-        val = collatz_eval(45, 60)
-        self.assertEqual(val, 113)
-
-    def test_eval_12(self):
-        val = collatz_eval(1, 999)
-        self.assertEqual(val, 179)
-
-    def test_eval_13(self):
-        val = collatz_eval(1000, 1099)
-        self.assertEqual(val, 169)
-
-    def test_eval_14(self):
-        val = collatz_eval(8889, 9000)
-        self.assertEqual(val, 247)
-
-    def test_eval_15(self):
-        val = collatz_eval(42, 69)
-        self.assertEqual(val, 113)
-
-    def test_eval_16(self):
-        val = collatz_eval(3, 3)
-        self.assertEqual(val, 8)
-
-    def test_eval_17(self):
-        val = collatz_eval(123456, 123457)
-        self.assertEqual(val, 88)
-
-    def test_eval_18(self):
-        val = collatz_eval(1050, 1350)
-        self.assertEqual(val, 182)
-
-    def test_eval_19(self):
-        val = collatz_eval(999, 1001)
-        self.assertEqual(val, 143)
-
-    def test_eval_20(self):
-        val = collatz_eval(1, 1)
-        self.assertEqual(val, 1)
+        """Testing max cycle"""
+        val = collatz_eval(443846, 945884)
+        self.assertEqual(val, 525)
 
     # -----
     # print
     # -----
 
-    def test_print(self):
+    def test_print_1(self):
+        """Testing print"""
         inputoutput = StringIO()
         collatz_print(inputoutput, 1, 10, 20)
         self.assertEqual(inputoutput.getvalue(), "1 10 20\n")
+
+    def test_print_2(self):
+        """Testing print"""
+        inputoutput = StringIO()
+        collatz_print(inputoutput, 2, 11, 21)
+        self.assertEqual(inputoutput.getvalue(), "2 11 21\n")
+
+    def test_print_3(self):
+        """Testing print"""
+        inputoutput = StringIO()
+        collatz_print(inputoutput, 4, 5, 6)
+        self.assertEqual(inputoutput.getvalue(), "4 5 6\n")
 
     # -----
     # solve
     # -----
 
     def test_solve(self):
+        """Testing solve"""
         read = StringIO("1 10\n100 200\n201 210\n900 1000\n")
         write = StringIO()
         collatz_solve(read, write)
