f11707e6a630f5620c7973e18f0e0a17ec7fbd7c
Denise Ly <dply95@gmail.com>
2016-09-09 02:46:57 +0000

    Removed unnecessary imports

--- dl27458-TestCollatz.py
+++ dl27458-TestCollatz.py
@@ -15,7 +15,7 @@
 from io       import StringIO
 from unittest import main, TestCase
 
-from Collatz import collatz_read, collatz_eval, collatz_print, collatz_solve, load_metacache, get_ranges_to_calc, get_lengths_to_check_against
+from Collatz import collatz_read, collatz_eval, collatz_print, collatz_solve
 
 # -----------
 # TestCollatz
